201be31af4760b2e5892207f51d994105a87d18a
Hlaing Min Oo <52852283+Williamhmo@users.noreply.github.com>
2024-07-22 13:32:03 +0630

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 import keras
-from keras.layers import TextVectorization; import tokenizer
+from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
 import pickle
 import pandas as pd
